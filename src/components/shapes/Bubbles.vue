036828f4e8a87f11b265253dc3a19d56b54f66bc
Dusan Mijatovic (myLin2019) <d.mijatovic@dv4all.nl>
2019-09-30 20:49:13 +0200

    bubbles component

--- src/components/shapes/Bubbles.vue
+++ src/components/shapes/Bubbles.vue
@@ -1,33 +1,37 @@
 <template>
   <svg>
     <circle
-      v-for="(item,index) in data"
+      v-for="(item,index) in dataset"
       :key="index"
       :cx="item.cx"
       :cy="item.cy"
       :r="item.r"
-      fill="var(--color-dark)"
+      :fill="item.fill"
       @click="()=>onClick(index)"
-    ></circle>
+    />
   </svg>
 </template>
 
 <script>
 export default {
-  name: "Bubbles",
+  name: 'Bubbles',
   props: {
     dataset: {
       type: Array,
-      default: () => []
-    }
+      default: () => [],
+    },
   },
   methods: {
     onClick(i) {
-      console.log("user clicked item...", i);
-    }
-  }
-};
+      console.log('user clicked item...', i)
+    },
+  },
+}
 </script>
 
-<style>
+<style scoped>
+svg {
+  width: 100%;
+  min-height: 400px;
+}
 </style>
---
+++ src/pages/Line1.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>Line graph</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LinePage',
+}
+</script>
+
+<style>
+</style>
